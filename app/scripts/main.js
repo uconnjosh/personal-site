245418945033220491332a9fd5eb7388c33e5bf7
Joshua Paul <joshuapaul@Joshuas-MacBook-Pro.local>
2018-05-28 23:47:22 -0400

    Add a little animation to hero section. Begin markup and styles for the resume section and start placing some content.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,41 @@
 $(document).ready(function() {
   addScrollListener()
+  addClickListeners()
 })
 
-function addScrollListener() {
-	var josh = $('#greeting__josh');
-	var paul = $('#greeting__paul');
+ var addScrollListener = () => {
+	const josh = $('#greeting__josh')
+	const paul = $('#greeting__paul')
   $(document).on("scroll", function() {
-  	var scrollPosition = $(document).scrollTop()
-  	var _css = scrollPosition + 'px'
-  	console.log(_css);
+  	let scrollPosition = $(document).scrollTop()
+  	let _css = scrollPosition + 'px'
+    
     josh.css({'margin-right': _css})
     paul.css({'margin-left': _css})
   })
+}
+
+
+// $('#resume--skills').children().find('.resume__section')
+var addClickListeners = () => {	
+	const resumeSection = $('.resume__section')
+  // const resumeSkills = $('#resume--skills')
+  // const resumeEmployment = $('#resume--employment')
+  // const resumeProjects = $('#resume--projects')
+  // const resumeEducation = $('#resume--education')
+
+  resumeSection.on('click', function() {
+  	const section = $(this).find('.resume__details').first()
+  	toggleResumeSection(section)
+  })
+}
+
+var toggleResumeSection = (section) => {
+	// TODO: hold state in an object to make this logic more stable
+	if (section.css('max-height') != '0px') {
+	  section.css('max-height', '0')
+	  return	
+	}
+
+  section.css('max-height', '200px');
 }
