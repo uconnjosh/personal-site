3dbd43c6dea5c274c3bef1523cf42030d4bed27c
Josh Paul <josh4044@gmail.com>
2018-05-31 08:34:10 -0700

    Tweak hero-fade out logic. Add a header name section that appears when you scroll down the page.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+let headerVisible = false
+
 $(document).ready(function() {
   addScrollListener()
   addClickListeners()
@@ -6,11 +8,39 @@ $(document).ready(function() {
 var addScrollListener = () => {
 	const heroImg = $('#hero__image');
   $(document).on("scroll", function() {
-    let scrollPosition = $(document).scrollTop()
-    let newOpacity = 1 - (scrollPosition / 700)
+    let scrollPercentage = $(document).scrollTop() / heroImg.height()
+    let newOpacity = 1 - (scrollPercentage)
+
+    if (scrollPercentage > .90) {
+      stickyHeader()
+      return
+    }
+
+    if (headerVisible) {
+    	unStickHeader()
+    }
     
     heroImg.css({'opacity': newOpacity})
   })
+}
+
+// TODO: refactor using position: sticky
+var stickyHeader = () => {
+  const hero = $('.hero')
+  const headerName = $('.header__name')
+
+  hero.css({'position': ' sticky', 'top': '-85vh'})
+  headerName.css({'opacity': '1'})
+  headerVisible = true
+}
+
+var unStickHeader = () => {
+  const hero = $('.hero')
+  const headerName = $('.header__name')
+
+  hero.css({'position': 'relative', 'top': '0'})
+  headerName.css({'opacity': '0'})
+  headerVisible = false
 }
 
 var addClickListeners = () => {	
